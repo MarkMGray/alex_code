ed54fa9161bae6165f5141ac73ec581842727ff8
Alex Braylan <alexbraylan@gmail.com>
2015-11-25 12:01:06 -0600

    completion code

--- init.js
+++ init.js
@@ -26,10 +26,50 @@ el("finishButt").onclick = function() {
 	el("surveyDiv").style.display = "block"
 }
 
+function finishCode() {
+    var result = "";
+    var pop = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < 8; i++) result += pop.charAt(Math.floor(Math.random() * pop.length));
+    return result;
+}
+
 el("submitButt").onclick = function() {
 	DISPLAY.citizen.evaluation.surveyScore = getCheckedValue("survey");
 	DISPLAY.citizen.evaluation.endMs = (new Date()).getTime();
 	if (DISPLAY.citizen.evaluation.surveyScore) el("surveyDiv").style.display = "none"
 
 	console.log(DISPLAY.citizen.getSaveData()) // send this to server
+
+	var completionCode = finishCode();
+	var postObj = DISPLAY.citizen.getSaveData();
+	console.log(DISPLAY.citizen);
+	postObj.citizenID = citizenID.toString();
+	postObj.generationID = generationID.toString();
+	postObj.completionCode = completionCode;
+	console.log(postObj["cellData"]);
+	postObj = {"data" :  JSON.stringify(postObj)}
+	console.log("postobj: ");
+	console.log(postObj);
+
+	// $("#overlay").show();
+	// $.post("/api/save", postObj, function (data) {
+	// 	console.log(data);
+	// 	var jsonObj = $.parseJSON(data);
+
+	// 	if(jsonObj["response_code"] != 0) {
+	// 		toastr.error("Error: " + jsonObj["error_msg"]);
+	// 	} else {
+	// 		var tmp = toastr.options;
+	// 		toastr.success("Successfully saved response!");
+	// 		toastr.options.timeOut = 750000;
+	// 		toastr.options.positionClass = "toast-top-center";
+	// 		toastr.info("Your completion code is: " + completionCode);
+	// 		toastr.options = tmp;
+	// 	}
+	// 	$("#overlay").delay(500).hide(0);
+	// })
+	// 	.fail(function () {
+	// 		toastr.error("Failed to save response!");
+	// 		$("#overlay").delay(500).hide(0);
+	// });
 }
